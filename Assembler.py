597e53e8980603e8cb1ef6946b2977ee6f6dcce8
Sigma-V <119050103+Sigma-V@users.noreply.github.com>
2023-05-17 10:57:42 +0530

    Update Assembler.py

--- Assembler.py
+++ Assembler.py
@@ -165,4 +165,315 @@ def valid_memory(memory):
                 flag = "is not a valid memory address"
                 break
         return flag
+reg_codes = {"R0" : "000","R1" : "001", "R2" : "010" , "R3" : "011" , "R4" : "100" , "R5" : "101" , "R6" :"110", "FLAGS" : "111"}
+isa_codes = {
+        "add" : {"opcode" : "00000", "type" : "a"},
+        "sub" : {"opcode" : "00001", "type" : "a"},
+        "movi" : {"opcode" : "00010", "type" : "b"},
+        "movr" : {"opcode" : "00011", "type" : "c"},
+        "ld" : {"opcode" : "00100", "type" : "d"},
+        "st" : {"opcode" : "00101", "type" : "d"},
+        "mul" : {"opcode" : "00110", "type" : "a"},
+        "div" : {"opcode" : "00111", "type" : "c"},
+        "rs" : {"opcode" : "01000", "type" : "b"},
+        "ls" : {"opcode" : "01001", "type" : "b"},
+        "xor" : {"opcode" : "01010", "type" : "a"},
+        "or" : {"opcode" : "01011", "type" : "a"},
+        "and" : {"opcode" : "01100", "type" : "a"},
+        "not" : {"opcode" : "01101", "type" : "c"},
+        "cmp" : {"opcode" : "01110", "type" : "c"},
+        "jmp" : {"opcode" : "01111", "type" : "e"},
+        "jlt" : {"opcode" : "11100", "type" : "e"},
+        "jgt" : {"opcode" : "11101", "type" : "e"},
+        "je" : {"opcode" : "11111", "type" : "e"},
+        "hlt" : {"opcode" : "11010", "type" : "f"}}
 
+list_of_instructions = list(isa_codes.keys()) 
+
+list_of_registers = list(reg_codes.keys())
+
+        
+data = sys.stdin.readlines()
+data_1 = data.copy()
+
+for counter in range(len(data)):
+    data[counter]=data[counter].rstrip()
+    data[counter] = data[counter].split()
+
+no_of_lines = len(data)
+list_of_variables = []                              
+line_when_variable_added = []                       
+for counter in range(len(data)):
+    if data[counter][0] == "var":
+        list_of_variables.append(data[counter][1])
+        line_when_variable_added.append(counter+1)
+no_of_variables = len(list_of_variables)
+
+input_labels = []                                  
+jump_to_labels = []                                      
+input_register = []
+input_memory = []
+input_instruction = []
+immediates = []
+
+for counter in range(len(data)):   
+    try:                                                                                            
+        if data[counter][0][len(data[counter][0])-1] == ":":                                        
+            input_labels.append(data[counter][0][0:len(data[counter][0])-1:])                        
+            input_instruction.append(data[counter][1])                                              
+            if data[counter][1] == "mov"or data[counter][1] == "movi" or data[counter][1] == "movr":
+                if data[counter][3] in list_of_registers:
+                    input_register.append(data[counter][2])
+                    input_register.append(data[counter][3])
+                else:
+                    input_register.append(data[counter][2])
+                    immediates.append(data[counter][3][1::])
+            elif isa_codes[data[counter][1]]["type"] == "a":                                                                  
+                input_register.append(data[counter][2])
+                input_register.append(data[counter][3])
+                input_register.append(data[counter][4])
+            elif isa_codes[data[counter][1]]["type"] == "b":
+                input_register.append(data[counter][2])
+                immediates.append(data[counter][3][1::])
+            elif isa_codes[data[counter][1]]["type"] == "c":
+                input_register.append(data[counter][2])
+                input_register.append(data[counter][3]) 
+            elif isa_codes[data[counter][1]]["type"] == "d":
+                input_register.append(data[counter][2])
+                input_memory.append(data[counter][3])
+            elif isa_codes[data[counter][1]]["type"] == "e":
+                jump_to_labels.append(data[counter][2])
+            else:
+                continue
+        else:
+            input_instruction.append(data[counter][0])
+            if data[counter][0] == "mov" or data[counter][0] == "movi" or data[counter][0] == "movr" :
+                if data[counter][2] in list_of_registers:
+                    input_register.append(data[counter][1])
+                    input_register.append(data[counter][2])
+                else:
+                    input_register.append(data[counter][1])
+                    immediates.append(data[counter][2][1::])
+            elif isa_codes[data[counter][0]]["type"] == "a":
+                    input_register.append(data[counter][1])
+                    input_register.append(data[counter][2])
+                    input_register.append(data[counter][3])
+            elif isa_codes[data[counter][0]]["type"] == "b":
+                input_register.append(data[counter][1])
+                immediates.append(data[counter][2][1::])
+            elif isa_codes[data[counter][0]]["type"] == "c":
+                input_register.append(data[counter][1])
+                input_register.append(data[counter][2]) 
+            elif isa_codes[data[counter][0]]["type"] == "d":
+                input_register.append(data[counter][1])
+                input_memory.append(data[counter][2])
+            elif isa_codes[data[counter][0]]["type"] == "e":
+                jump_to_labels.append(data[counter][1])
+            else:
+                continue
+    except:
+            continue
+
+if variable_starting(no_of_variables,line_when_variable_added) == False:
+    print("Error: Variables must be declared at the beginning")
+
+if correct_usage_of_halt(input_instruction) != True:
+    print(correct_usage_of_halt(input_instruction))
+
+pussy = 1
+
+for i in range(no_of_lines):
+    base = 0
+    if data[i][0][len(data[i][0])-1] == ":":
+        base += 1
+    instruct = data[i][base]
+    if instruct == "mov":
+        if data[i][base+2][0] == "$":
+            instruct = "movi"
+        else:
+            instruct = "movr"
+    if instruct == "var":
+        continue
+    elif instruct not in list_of_instructions:
+        print(f"Error in Line-{i+1}: {instruct} is not defined")
+        pussy = 0
+    else:
+        typ = isa_codes[instruct]["type"]
+        if typ == "a":
+            nos = len(data[i]) - base
+            if nos != 4:
+                print(f"Error in Line-{i+1}: Invalid number of registers")
+                pussy = 0
+            else:
+                if data[i][base+1] not in list_of_registers:
+                    print(f"Error in Line-{i+1}: {data[i][base+1]} is not a valid register")
+                    pussy = 0
+                if data[i][base+2] not in list_of_registers:
+                    print(f"Error in Line-{i+1}: {data[i][base+2]} is not a valid register")
+                    pussy = 0
+                if data[i][base+3] not in list_of_registers:
+                    print(f"Error in Line-{i+1}: {data[i][base+3]} is not a valid register")
+                    pussy = 0
+        elif typ == "b":
+            nos = len(data[i]) - base
+            if nos != 3:
+                print(f"Error in Line-{i+1}: Invalid number of arguments")
+                pussy = 0
+            else:
+                if data[i][base+1] not in list_of_registers:
+                    print(f"Error in Line-{i+1}: {data[i][base+1]} is not a valid register")
+                    pussy = 0
+                if data[i][base+2][0] != "$":
+                    print(f"Error in Line-{i+1}: {data[i][base+2][0]} is not a valid immediate")
+                    pussy = 0
+                else:
+                    if valid_immediate(data[i][base+2]) != True:
+                        print(f"Error in Line-{i+1}: {data[i][base+2]} {valid_immediate(data[i][base+2])}")
+                        pussy = 0
+        elif typ == "c":
+            nos = len(data[i]) - base
+            if nos != 3:
+                print(f"Error in Line-{i+1}: Invalid number of arguments")
+                pussy = 0
+            else:
+                if instruct == "movr":
+                    if data[i][base+1] not in list_of_registers:
+                        print(f"Error in Line-{i+1}: {data[i][base+1]} is not a valid register")
+                        pussy = 0
+                    if data[i][base+2] not in list_of_registers and data[i][base+2] != "FLAGS":
+                        print(f"Error in Line-{i+1}: {data[i][base+2]} is not a valid register")
+                        pussy = 0
+                else:
+                    if data[i][base+1] not in list_of_registers:
+                        print(f"Error in Line-{i+1}: {data[i][base+1]} is not a valid register")
+                        pussy = 0
+                    if data[i][base+2] not in list_of_registers:
+                        print(f"Error in Line-{i+1}: {data[i][base+2]} is not a valid register")
+                        pussy = 0
+        elif typ == "d":
+            nos = len(data[i]) - base
+            if nos != 3:
+                print(f"Error in Line-{i+1}: Invalid number of arguments")
+                pussy = 0
+            else:
+                if data[i][base+1] not in list_of_registers:
+                    print(f"Error in Line-{i+1}:{data[i][base+1]} is not a valid register ")
+                    pussy = 0
+                if data[i][base + 2] not in list_of_variables and valid_memory(data[i][base + 2]) != True:
+                    print(f"Error in Line-{i+1}: {data[i][base+2]} is not a valid variable name or memory location")
+                    pussy = 0
+        elif typ == "e":
+            nos = len(data[i]) - base
+            if nos != 2:
+                print(f"Error in Line-{i+1}: Invalid number of arguments")
+                pussy = 0
+            else:
+                if data[i][base+1] not in input_labels and valid_memory(data[i][base + 1]) != True :
+                    print(f"Error in Line-{i+1}: {data[i][base+1]} is not a valid address")
+                    pussy = 0
+        elif typ == "f":
+            nos = len(data[i]) - base
+            if nos != 1:
+                print(f"Error in Line-{i+1}: Invalid number of arguments")
+                pussy = 0
+
+if pussy == 1:
+    m= []
+    for i in range(0,127) :
+        x = str(bin(i)).lstrip("0b")
+        m.append(x)
+
+    label_checker = {}         
+    var_name = {}              
+    count_counter = var_count = count_empty = 0
+
+    for i in data_1:
+        if i != "\n":
+            final_list = [x for x in i.split()]
+            if final_list[0][-1] == ":":
+                final_list = final_list[1:]
+            if final_list != []:
+                if final_list[0] == "var":
+                    var_count += 1
+            else:
+                count_empty += 1
+        else:
+            count_empty += 1
+
+    length = len(data_1) - var_count - count_empty
+
+    for i in data_1:
+        if i == "\n":
+            continue
+        else:
+            final_list = [x for x in i.split()]
+            if final_list[0] == "var" :
+                var_name[final_list[1]] = m[count_counter + length]   
+            if final_list[0][-1] == ":" :
+                label_checker[final_list[0][0:-1]] = m[count_counter - var_count]
+                final_list = final_list[1:]
+            if final_list != []:
+                count_counter += 1
+
+    test = []
+    for i in data_1:
+        flag = True
+        val = ""
+        if i == "\n":
+            flag = False
+            pass
+        else:
+            final_list = [x for x in i.split()]
+            if final_list[0] == "var":
+                flag = False
+                pass
+            if final_list[0][-1] == ":" :
+                final_list = final_list[1:]
+            if final_list == []:
+                flag = False
+                pass
+            elif final_list[0] == "add":
+                val = add(final_list[1],final_list[2],final_list[3])
+            elif final_list[0] == "sub":
+                val = sub(final_list[1],final_list[2],final_list[3])
+            elif final_list[0] == "mov":
+                t = True
+                if final_list[2][0] == "$":
+                    t = False
+                val = mov(final_list[1],final_list[2],t)
+            elif final_list[0] == "ld":
+                temp=var_name[final_list[2]]
+                val = ld(final_list[1],temp)
+            elif final_list[0] == "st":
+                temp=var_name[final_list[2]]
+                val = st(final_list[1],temp)
+            elif final_list[0] == "mul":
+                val = mul(final_list[1],final_list[2],final_list[3])
+            elif final_list[0] == "div":
+                val = div(final_list[1],final_list[2])
+            elif final_list[0] == "rs":
+                val = rs(final_list[1],final_list[2])
+            elif final_list[0] == "ls":
+                val = ls(final_list[1],final_list[2])
+            elif final_list[0] == "xor":
+                val = xor(final_list[1],final_list[2],final_list[3])
+            elif final_list[0] == "or":
+                val = or_(final_list[1],final_list[2],final_list[3])
+            elif final_list[0] == "and":
+                val = and_(final_list[1],final_list[2],final_list[3])
+            elif final_list[0] == "not":
+                val = not_(final_list[1],final_list[2])
+            elif final_list[0] == "cmp":
+                val = cmp(final_list[1],final_list[2])
+            elif final_list[0] == "jmp":
+                val = jmp(final_list[1])
+            elif final_list[0] == "jlt":
+                val = jlt(final_list[1])
+            elif final_list[0] == "jgt":
+                val = jgt(final_list[1])
+            elif final_list[0] == "je":
+                val = je(final_list[1])
+            elif final_list[0] == "hlt":
+                val = hlt()
+        if flag == True:
+            print(val)
