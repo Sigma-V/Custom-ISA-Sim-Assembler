ecfb4befd7464e82a1ee1aecd8de36ff0b53c4ad
Shashwat Jha <119053126+Shash-03@users.noreply.github.com>
2023-06-06 20:52:50 +0530

    Update Assembler.py

--- Co_Project_B32/CO_A_P1/Simple-Assembler/Assembler.py
+++ Co_Project_B32/CO_A_P1/Simple-Assembler/Assembler.py
@@ -165,6 +165,8 @@ def valid_memory(memory):
                 flag = "is not a valid memory address"
                 break
         return flag
+
+
 reg_codes = {"R0" : "000","R1" : "001", "R2" : "010" , "R3" : "011" , "R4" : "100" , "R5" : "101" , "R6" :"110", "FLAGS" : "111"}
 isa_codes = {
         "add" : {"opcode" : "00000", "type" : "a"},
@@ -194,6 +196,8 @@ list_of_registers = list(reg_codes.keys())
 
         
 data = sys.stdin.readlines()
+data_1 = data.copy()
+
 data_1 = data.copy()
 
 for counter in range(len(data)):
@@ -274,13 +278,15 @@ for counter in range(len(data)):
     except:
             continue
 
+pussy = 1
 if variable_starting(no_of_variables,line_when_variable_added) == False:
+    pussy = 0
     print("Error: Variables must be declared at the beginning")
 
 if correct_usage_of_halt(input_instruction) != True:
+    pussy = 0
     print(correct_usage_of_halt(input_instruction))
 
-pussy = 1
 
 for i in range(no_of_lines):
     base = 0
@@ -378,7 +384,8 @@ for i in range(no_of_lines):
                 pussy = 0
 
 if pussy == 1:
-    
+    #varun pussy execute (not found)
+    #binary encoding
     m= []
     for i in range(0,127) :
         x = str(bin(i)).lstrip("0b")
